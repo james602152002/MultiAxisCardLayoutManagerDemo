5fc10d34f4189e2d82ed5e72da0106e7ce203fa9
= <=>
2018-02-28 15:38:10 +0800

    develop

--- app/src/main/java/com/james602152002/multiaxiscardlayoutmanagerdemo/MainApplication.java
+++ app/src/main/java/com/james602152002/multiaxiscardlayoutmanagerdemo/MainApplication.java
@@ -2,6 +2,7 @@ package com.james602152002.multiaxiscardlayoutmanagerdemo;
 
 import android.app.Application;
 import android.content.Context;
+import android.support.multidex.MultiDex;
 
 /**
  * Created by shiki60215 on 18-2-28.
@@ -11,6 +12,6 @@ public class MainApplication extends Application {
     @Override
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
-//        MultiDex.install(this);
+        MultiDex.install(this);
     }
 }

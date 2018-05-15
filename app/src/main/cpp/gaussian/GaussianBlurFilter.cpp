89644d8d986f3b27ffddc64cc16a5153d9dcd757
james602152002 <james602152002@hotmail.com>
2018-05-15 11:16:38 +0800

    update

--- app/src/main/cpp/gaussian/GaussianBlurFilter.cpp
+++ app/src/main/cpp/gaussian/GaussianBlurFilter.cpp
@@ -5,12 +5,27 @@
 #include <cstring>
 #include <android/bitmap.h>
 #include <stdio.h>
-#include <android/log.h>
+#include <stdlib.h>
 #include <math.h>
+#include <android/log.h>
 #include "GaussianBlurFilter.h"
+#include <time.h>
+#include <thread>
 
 #define LOG_TAG "cpp"
 #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG,LOG_TAG,__VA_ARGS__)
+
+struct thread_args {
+    int *scl;
+    int *tcl;
+    int w;
+    int h;
+    int r;
+};
+
+struct thread_args thread_r;
+struct thread_args thread_g;
+struct thread_args thread_b;
 
 GaussianBlurFilter *GaussianBlurFilter::instance;
 
@@ -20,108 +35,209 @@ GaussianBlurFilter *GaussianBlurFilter::getInstance() {
     return instance;
 }
 
+
+int *boxesForGauss(double sigma, int n)  // standard deviation, number of boxes
+{
+    double wIdeal = sqrt((12 * sigma * sigma / n) + 1);  // Ideal averaging filter width
+    int wl = (int) floor(wIdeal);
+    if (wl % 2 == 0) wl--;
+    int wu = wl + 2;
+
+    double mIdeal = (12 * sigma * sigma - n * wl * wl - 4 * n * wl - 3 * n) / (-4 * wl - 4);
+    int m = (int) round(mIdeal);
+    // var sigmaActual = Math.sqrt( (m*wl*wl + (n-m)*wu*wu - n)/12 );
+
+    int *sizes = new int[n];
+    for (int i = 0; i < n; i++) sizes[i] = i < m ? wl : wu;
+    return sizes;
+}
+
+
+void boxBlurH_3(int *scl, int *tcl, int w, int h, int r) {
+    double iarr = (double) 1 / (r + r + 1);
+    for (int i = 0; i < h; i++) {
+        int ti = i * w, li = ti, ri = ti + r;
+        int fv = scl[ti], lv = scl[ti + w - 1], val = (r + 1) * fv;
+        for (int j = 0; j < r; j++) val += scl[ti + j];
+        for (int j = 0; j <= r; j++) {
+            val += scl[ri++] - fv;
+            tcl[ti++] = (int) (val * iarr);
+        }
+        for (int j = r + 1; j < w - r; j++) {
+            val += scl[ri++] - scl[li++];
+            tcl[ti++] = (int) (val * iarr);
+        }
+        for (int j = w - r; j < w; j++) {
+            val += lv - scl[li++];
+            tcl[ti++] = (int) (val * iarr);
+        }
+    }
+}
+
+void boxBlurT_3(int *scl, int *tcl, int w, int h, int r) {
+    double iarr = (double) 1 / (r + r + 1);
+    for (int i = 0; i < w; i++) {
+        int ti = i, li = ti, ri = ti + r * w;
+        int fv = scl[ti], lv = scl[ti + w * (h - 1)], val = (r + 1) * fv;
+        for (int j = 0; j < r; j++) val += scl[ti + j * w];
+        for (int j = 0; j <= r; j++) {
+            val += scl[ri] - fv;
+            tcl[ti] = (int) (val * iarr);
+            ri += w;
+            ti += w;
+        }
+        for (int j = r + 1; j < h - r; j++) {
+            val += scl[ri] - scl[li];
+            tcl[ti] = (int) (val * iarr);
+            li += w;
+            ri += w;
+            ti += w;
+        }
+        for (int j = h - r; j < h; j++) {
+            val += lv - scl[li];
+            tcl[ti] = (int) (val * iarr);
+            li += w;
+            ti += w;
+        }
+    }
+}
+
+void boxBlur_3(int *scl, int *tcl, int w, int h, int r) {
+    int size = w * h;
+    for (int i = 0; i < size; i++) tcl[i] = scl[i];
+    boxBlurH_3(tcl, scl, w, h, r);
+    boxBlurT_3(scl, tcl, w, h, r);
+}
+
+void gaussBlur_3(int *scl, int *tcl, int w, int h, int r) {
+    int *bxs = boxesForGauss(r, 3);
+    boxBlur_3(scl, tcl, w, h, (bxs[0] - 1) / 2);
+    boxBlur_3(tcl, scl, w, h, (bxs[1] - 1) / 2);
+    boxBlur_3(scl, tcl, w, h, (bxs[2] - 1) / 2);
+}
+
+void *threadGaussianBlur(void *params) {
+    gaussBlur_3(((struct thread_args *) params)->scl, ((struct thread_args *) params)->tcl,
+                ((struct thread_args *) params)->w, ((struct thread_args *) params)->h,
+                ((struct thread_args *) params)->r);
+    pthread_exit((void *) 0);
+}
+
 void GaussianBlurFilter::startGaussianBlur(JniBitmap *jniBitmap, double sigma, int radius) {
     storedBitmapPixels = jniBitmap->_storedBitmapPixels;
     mImageWidth = jniBitmap->_bitmapInfo.width;
     mImageHeight = jniBitmap->_bitmapInfo.height;
 
     int img_size = mImageWidth * mImageHeight;
-    if (mImageData_rgb == NULL) {
-        mImageData_rgb = new uint32_t[img_size];
+
+    pthread_t r_id, g_id, b_id;
+
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+
+    int start_time = (int) tv.tv_sec;
+
+    int *distributeColorRed = new int[mImageHeight * mImageWidth];
+    int *distributeColorGreen = new int[mImageHeight * mImageWidth];
+    int *distributeColorBlue = new int[mImageHeight * mImageWidth];
+    int *_distributeColorRed = new int[mImageHeight * mImageWidth];
+    int *_distributeColorGreen = new int[mImageHeight * mImageWidth];
+    int *_distributeColorBlue = new int[mImageHeight * mImageWidth];
+    for (int i = 0; i < mImageHeight * mImageWidth; i++) {
+        int color = storedBitmapPixels[i];
+        int r = (color & 0xFF0000) >> 16;
+        int g = (color & 0xFF00) >> 8;
+        int b = color & 0xFF;
+        distributeColorRed[i] = r;
+        distributeColorGreen[i] = g;
+        distributeColorBlue[i] = b;
     }
-    memcpy(mImageData_rgb, storedBitmapPixels, sizeof(uint32_t) * img_size);
 
-    int filter_width = (radius << 1) + 1;
-    int filter_size = filter_width * filter_width;
-    int *r;
-    int *g;
-    int *b;
+    thread_r.scl = distributeColorRed;
+    thread_r.tcl = _distributeColorRed;
+    thread_r.w = mImageWidth;
+    thread_r.h = mImageHeight;
+    thread_r.r = radius;
+    thread_g.scl = distributeColorGreen;
+    thread_g.tcl = _distributeColorGreen;
+    thread_g.w = mImageWidth;
+    thread_g.h = mImageHeight;
+    thread_g.r = radius;
+    thread_b.scl = distributeColorBlue;
+    thread_b.tcl = _distributeColorBlue;
+    thread_b.w = mImageWidth;
+    thread_b.h = mImageHeight;
+    thread_b.r = radius;
 
-    double *distributeColorRed = new double[filter_size];
-    double *distributeColorGreen = new double[filter_size];
-    double *distributeColorBlue = new double[filter_size];
+    pthread_create(&r_id, NULL, threadGaussianBlur, (void *) &thread_r);
+    pthread_create(&g_id, NULL, threadGaussianBlur, (void *) &thread_g);
+    pthread_create(&b_id, NULL, threadGaussianBlur, (void *) &thread_b);
+    pthread_join(r_id, NULL);
+    pthread_join(g_id, NULL);
+    pthread_join(b_id, NULL);
 
-    normalizeFilter(sigma, filter_width);
-
-    float lightness = (float) pow(radius, 0.2f);
-
-    for (int h = 0; h < mImageHeight; h++) {
-        for (int w = 0; w < mImageWidth; w++) {
-            for (int j = 0; j < filter_size; j++) {
-                int x = (j % filter_width) + w;
-                int y = (j / filter_width) + h;
-                if (x >= mImageWidth || y >= mImageHeight) {
-                    distributeColorRed[j] = 0;
-                    distributeColorGreen[j] = 0;
-                    distributeColorBlue[j] = 0;
-                    continue;
-                }
-                int color = storedBitmapPixels[x + y * mImageWidth];
-                int _r = (int) (((color & 0xFF0000) >> 16) * lightness);
-                int _g = (int) (((color & 0xFF00) >> 8) * lightness);
-                int _b = (int) ((color & 0xFF) * lightness);
-                r = &_r;
-                g = &_g;
-                b = &_b;
-                distributeColorRed[j] = filter[j] * *r;
-                distributeColorGreen[j] = filter[j] * *g;
-                distributeColorBlue[j] = filter[j] * *b;
-            }
-            int _r = generateDistributeColorInPixel(distributeColorRed, filter_size);
-            int _g = generateDistributeColorInPixel(distributeColorGreen, filter_size);
-            int _b = generateDistributeColorInPixel(distributeColorBlue, filter_size);
-            r = &_r;
-            g = &_g;
-            b = &_b;
-            storedBitmapPixels[h * mImageWidth + w] = 0xFF000000 + (*r << 16) + (*g << 8) + *b;
-        }
-        LOGD("h ========= %d", h);
+    for (int i = 0; i < img_size; i++) {
+        storedBitmapPixels[i] =
+                0xFF000000 + (_distributeColorRed[i] << 16) + (_distributeColorGreen[i] << 8) +
+                _distributeColorBlue[i];
     }
+
+    gettimeofday(&tv, NULL);
+    LOGD("r = %d, w = %d, h = %d, time spent %ds", radius, mImageWidth, mImageHeight,
+         ((int) tv.tv_sec - start_time));
+
+
     delete distributeColorRed;
     distributeColorRed = NULL;
     delete distributeColorGreen;
     distributeColorGreen = NULL;
     delete distributeColorBlue;
     distributeColorBlue = NULL;
+    delete _distributeColorRed;
+    _distributeColorRed = NULL;
+    delete _distributeColorGreen;
+    _distributeColorGreen = NULL;
+    delete _distributeColorBlue;
+    _distributeColorBlue = NULL;
 }
 
-void GaussianBlurFilter::normalizeFilter(double sigma, int filter_size) {
-    filter = new double[filter_size * filter_size];
-    int semi_width = (filter_size - 1) >> 1;
-    double r, s = 2.0 * sigma * sigma;
-
-    // sum is for normalization
-    double sum = 0.0;
-
-    // generating filter_size x filter_size kernel
-    int i = 0;
-    for (int y = -semi_width; y <= semi_width; y++) {
-        for (int x = -semi_width; x <= semi_width; x++) {
-            r = sqrt(y * y + x * x);
-            filter[i] = (exp(-(r * r) / s)) / (M_PI * s);
-            sum += filter[i];
-            i++;
-        }
-    }
-
-    // normalising the Kernel
-    i = 0;
-    for (int y = 0; y < filter_size; y++) {
-        for (int x = 0; x < filter_size; x++) {
-            filter[i] /= sum;
-            i++;
-        }
-    }
-}
-
-int GaussianBlurFilter::generateDistributeColorInPixel(double *distribute_color, int filter_size) {
-    int summation = 0;
-    for (int x = 0; x < filter_size; x++) {
-        summation += distribute_color[x];
-    }
-    summation = summation > 255 ? 255 : summation;
-    return summation;
-}
+//void GaussianBlurFilter::normalizeFilter(double sigma, int filter_size) {
+//    filter = new double[filter_size * filter_size];
+//    int semi_width = (filter_size - 1) >> 1;
+//    double r, s = 2.0 * sigma * sigma;
+//
+//    // sum is for normalization
+//    double sum = 0.0;
+//
+//    // generating filter_size x filter_size kernel
+//    int i = 0;
+//    for (int y = -semi_width; y <= semi_width; y++) {
+//        for (int x = -semi_width; x <= semi_width; x++) {
+//            r = sqrt(y * y + x * x);
+//            filter[i] = (exp(-(r * r) / s)) / (M_PI * s);
+//            sum += filter[i];
+//            i++;
+//        }
+//    }
+//
+//    // normalising the Kernel
+//    i = 0;
+//    for (int y = 0; y < filter_size; y++) {
+//        for (int x = 0; x < filter_size; x++) {
+//            filter[i] /= sum;
+//            i++;
+//        }
+//    }
+//}
+//
+//int GaussianBlurFilter::generateDistributeColorInPixel(double *distribute_color, int filter_size) {
+//    int summation = 0;
+//    for (int x = 0; x < filter_size; x++) {
+//        summation += distribute_color[x];
+//    }
+//    summation = summation > 255 ? 255 : summation;
+//    return summation;
+//}
 
 void GaussianBlurFilter::unInit() {
     if (instance != NULL)
--- app/src/main/cpp/gaussian/GaussianBlurFilter.h
+++ app/src/main/cpp/gaussian/GaussianBlurFilter.h
@@ -14,20 +14,32 @@ public:
 
     void startGaussianBlur(JniBitmap *jniBitmap, double sigma, int radius);
 
-    void normalizeFilter(double sigma, int filter_size);
-
-    int generateDistributeColorInPixel(double *distribute_color, int filter_width);
+//    void normalizeFilter(double sigma, int filter_size);
+//
+//    int generateDistributeColorInPixel(double *distribute_color, int filter_width);
 
     void unInit();
 
 private:
     static GaussianBlurFilter *instance;
     uint32_t *storedBitmapPixels;
-    uint32_t *mImageData_rgb;
-    double *filter;
+//    uint32_t *mImageData_rgb;
+//    double *filter;
 
     int mImageWidth;
     int mImageHeight;
+
+//    void *threadGaussianBlur(void *params);
+//
+//    void gaussBlur_3(int* scl, int* tcl, int w, int h, int r);
+//
+//    void boxBlur_3(int* scl,int* tcl,int w,int h, int r);
+//
+//    void boxBlurH_3(int *scl, int *tcl, int w, int h, int r);
+//
+//    void boxBlurT_3(int *scl, int *tcl, int w, int h, int r);
+//
+//    int *boxesForGauss(double sigma, int n);
 };
 
 
